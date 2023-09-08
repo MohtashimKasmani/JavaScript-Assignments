0d08a7b5b41f72dbee4d4dc72f633a22ef47ee9d
Mohtashim Abdul Wahid <Mohtashimkasmani30@gmail.com>
2023-09-08 16:40:44 +0500

    Updated

---
+++ js/chap1.js
@@ -0,0 +1,15 @@
+//Question 1
+// alert("Hello World.!");
+
+//Question 2
+// alert("Error! Please enter the valid password.");
+
+//Question 3
+// alert("Welcome to JS Land \nHappy Coding!");
+
+//Question 4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+//Question 5
+//console.log('Hello...! I can run JS through my web browsers console');
